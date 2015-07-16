98ca23a693c3d31b0227135fef4649ccdb95a936
Elias Farah <elias.farah@cejam.org.br>
2015-07-16 19:10:30 -0300

    - Correção que impedia que a programação pudesse ser vista.

--- www/js/services.js
+++ www/js/services.js
@@ -1,36 +1,61 @@
+var remote_server = 'http://sis.cejam.org.br/simposio/';
 angular.module('simposio.services', ['ngResource','ionic.utils'])
 
-.factory('Palestrantes', function($resource, $http, $localstorage) {
+.factory('Palestrantes', function($resource, $http, $localstorage, $utils) {
 	
   return {
-    all: function(callback) {
-		$http.post('http://sis.cejam.org.br/simposio/palestrantes.php').success(callback);		
+    all: function(success_callback) {
+		  $http.post(remote_server+'palestrantes.php')
+      .success(success_callback)
+      .error(function(data, status, headers, config) {        
+        alert("Ooops, algo deu errado, verifique sua conexão e tente novamente!");                
+        $scope.$broadcast('scroll.refreshComplete');
+        $utils.hide();
+      });
     }
   }
 })
 
-.factory('Programacoes', function($resource, $http) {
+.factory('Programacoes', function($resource, $http, $localstorage, $utils) {
 	
   return {
-    all: function(callback) {
-		$http.post('http://sis.cejam.org.br/simposio/programacao.php').success(callback);
+    all: function(success_callback) {
+		  $http.post(remote_server+'programacao.php')
+      .success(success_callback)
+      .error(function(data, status, headers, config) {
+        alert("Ooops, algo deu errado, verifique sua conexão e tente novamente!");
+        $scope.$broadcast('scroll.refreshComplete');
+        $utils.hide();
+      });
     }
   }
 })
 
-.factory('Localizacao', function($resource, $http) {
+.factory('Localizacao', function($resource, $http, $localstorage, $utils) {
   return {
-    all: function(callback) {
-		$http.post('http://sis.cejam.org.br/simposio/localizacao.php').success(callback);
+    all: function(success_callback) {
+		  $http.post(remote_server+'localizacao.php')
+      .success(success_callback)
+      .error(function(data, status, headers, config) {
+        alert("Ooops, algo deu errado, verifique sua conexão e tente novamente!");
+        $scope.$broadcast('scroll.refreshComplete');
+        $utils.hide();
+      });
     }
   }
 })
 
-.factory('Sobre', function($resource, $http) {
+.factory('Sobre', function($resource, $http, $localstorage, $utils) {
 	
   return {
-    all: function(callback) {
-		$http.post('http://sis.cejam.org.br/simposio/sobre.php').success(callback);
+    all: function(success_callback) {
+		  $http.post(remote_server+'sobre.php')
+      .success(success_callback)
+      .error(function(data, status, headers, config) {
+        alert("Ooops, algo deu errado, verifique sua conexão e tente novamente!");
+        $scope.$broadcast('scroll.refreshComplete');
+        $utils.hide();
+      });
     }
   }
 });
